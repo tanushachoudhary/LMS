b3ecc5384a7c7e685a37c998f80cde741fb8cd8b
Tanusha Choudhary <144540796+tanushachoudhary@users.noreply.github.com>
2025-02-08 21:54:20 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import React from "react";
+import Home from "./pages/student/Home";
+import CoursesList from "./pages/student/CoursesList";
+import MyEnrollments from "./pages/student/MyEnrollments";
+import CourseDetails from "./pages/student/CourseDetails";
+import Player from "./pages/student/Player";
+import Loading from "./components/student/Loading";
+import Educator from "./pages/educator/Educator";
+import Dashboard from "./pages/educator/Dashboard";
+import AddCourse from "./pages/educator/AddCourse";
+import MyCourses from "./pages/educator/MyCourses";
+import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
+
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="/educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentsEnrolled />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/educator/Educator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+const Educator = () => {
+  return (
+    <div>
+      <h1>Educator Page</h1>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Educator
